da1ab91fa6a44f354575f9815a6e744a1fa16fe3
DiegoMarchandon <diegomarc2020@gmail.com>
2024-05-25 17:15:52 -0300

    simplifico divs + animacion hover

---
+++ main.js
@@ -0,0 +1,9 @@
+let title = document.getElementById('title');
+
+console.log(title);
+
+/* si queremos acceder al elemento por otro atributo más allá del id
+podemos usar querySelector o querySelectorAll */
+
+
+let 
